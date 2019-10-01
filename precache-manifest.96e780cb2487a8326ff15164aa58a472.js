1527e0f83902c6c3eeb8097fca6b13792d3f6b1c
Yeshwanth Jagannath <yeshwanth.jagannath@gmail.com>
2019-09-30 20:07:03 -0700

    Updates

---
+++ precache-manifest.96e780cb2487a8326ff15164aa58a472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e201cab2ca90186bb18870745c61eab6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d59fdc05f05ea780a1bf",
+    "url": "/monsters-rolodex/static/css/main.87403073.chunk.css"
+  },
+  {
+    "revision": "877ab33c4cabc00823c7",
+    "url": "/monsters-rolodex/static/js/2.4586650f.chunk.js"
+  },
+  {
+    "revision": "d59fdc05f05ea780a1bf",
+    "url": "/monsters-rolodex/static/js/main.00e23515.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
